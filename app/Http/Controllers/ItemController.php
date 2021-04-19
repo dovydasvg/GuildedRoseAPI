14a0cfb5e2633936f20cd99637c338c3c7515994
dovydasvg <dovydasvg@yahoo.fr>
2021-04-19 16:29:27 +0300

    add item validation method and store method

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -11,4 +11,29 @@ class ItemController extends Controller
     {
         return Item::all();
     }
+
+    public function store(Request $request)
+    {
+        $fields = $this->validateItem($request);
+
+        $item = Item::create([
+            'name' => $fields['name'],
+            'value' => $fields['value'],
+            'quality' => $fields['quality'],
+            'category_id' => $fields['category_id'],
+        ]);
+
+        return response($item,201);
+
+    }
+
+    public function validateItem(Request $request): array
+    {
+        return $request->validate([
+            'name' => 'required|string|min:5',
+            'value' => 'required|numeric|min:10|max:100',
+            'quality' => 'required|integer|min:-10|max:50',
+            'category_id' => 'exists:App\Models\Category,id'
+        ]);
+    }
 }
