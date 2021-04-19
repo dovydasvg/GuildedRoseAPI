c76f3e168da4558a5274689d45ed3eaa0ac2da6e
dovydasvg <dovydasvg@yahoo.fr>
2021-04-19 16:09:21 +0300

    Create items show all method

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
-    //
+    public function show_all()
+    {
+        return Item::all();
+    }
 }
